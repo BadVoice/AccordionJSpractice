aca26cfb224954f724ac414390bc7d24a0867bad
Herman <smurovnemez@mail.ru>
2022-06-11 11:29:59 +0300

    early-second-try-make-accordion

---
+++ js/modalClose.js
@@ -0,0 +1,18 @@
+const div = document.querySelector('.modal__inner');
+const modal = document.querySelector('.modal')
+
+
+let closeItemModal = document.createElement('div')
+closeItemModal.innerHTML = 'X'
+div.prepend(closeItemModal)
+closeItemModal.style.cssText = `
+display: flex;
+justify-content: flex-end;
+font-size: 24px;
+cursor: pointer;
+width: 20px;
+`
+
+closeItemModal.addEventListener('click', () => {
+    modal.style.display = '';
+})
